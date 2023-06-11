4e9e24afa4f683ddc94383ee444e356287617ef7
m-tonon <math.tonon@gmail.com>
2023-06-11 09:50:29 -0300

    feat: create skills-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroSectionComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    HeroSectionComponent,
+    SkillsSectionComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
---
+++ src/app/skills-section/skills-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skills-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills-section.component.html',
+  styleUrls: ['./skills-section.component.scss']
+})
+export class SkillsSectionComponent {
+
+}
